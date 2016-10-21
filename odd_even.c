a0676239382e5aeca220c8598441b7d06ef5645d
diwakar88 <damodarkandel435@gmail.com>
2016-10-21 11:35:31 +0545

    Add files via upload
    WAP to print odd and even Number.

---
+++ odd_even.c
@@ -0,0 +1,22 @@
+/*
+Student Name:Diwakar kandel
+Subject:Programming Fundamental
+Roll No:
+program:WAP to print odd & even number.
+Lab No:01
+Date:21oct,2016
+*/
+#include<stdio.h>
+int main()
+{
+   int a;
+   printf("Enter the number:\n");
+   scanf("%d",&a);
+   if(a/2){
+    printf("The number is odd of %d",a);
+    }
+   else{
+   printf("The number is even of %d",a);
+   }
+return 0;
+}
